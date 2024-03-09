cea17dca718bfd84a65c617207280854c7f3feb0
Salvador <sarevalotamayo@student.rtc.edu>
2024-03-08 22:12:37 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numbers = { 5, 12, 8, 88, 7 };
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+        Console.WriteLine("Sum: " + sum);
+    }
+}
